492dcd36e7415370e1f81a6c67fe91027635e8eb
Derek Corniello <corniedj@mail.uc.edu>
2025-01-04 16:24:36 -0500

    another fix for mobile overscroll

--- src/components/Starfield.vue
+++ src/components/Starfield.vue
@@ -21,11 +21,11 @@ export default {
 
 <style scoped>
 .starfield {
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
-  height: 150%;
+  height: 150vh;
   z-index: -1;
 }
 </style>
