555c4d350d126ba1ace0d71efea616c2482affd4
Derek Corniello <corniedj@mail.uc.edu>
2025-07-09 23:32:37 -0400

    stacked git stats

--- src/views/About.vue
+++ src/views/About.vue
@@ -30,9 +30,9 @@
                 <template #title>
                     📈 GitHub Stats:
                 </template>
-                <div style="display: flex; justify-content: center;">
-                    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=DerekCorniello&langs_count=8&layout=compact&exclude_repo=dotfiles,NeoVim-Setup&size_weight=0.5&count_weight=0.5&hide=MATLAB,HTML,CSS,JavaScript&theme=highcontrast&hide_border=true" alt="Top Languages" style="width: 35%; margin-right: 7.5%">
-                    <img src="https://github-readme-stats.vercel.app/api?username=derekcorniello&show_icons=true&hide=stars&count_private=true&hide_rank=true&theme=highcontrast&hide_border=true" alt="Derek's GitHub Stats" style="width: 35%; margin-left: 7.5%;">
+                <div style="display: flex; flex-direction: column; align-items: center; gap: 1.5rem;">
+                    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=DerekCorniello&langs_count=8&layout=compact&exclude_repo=dotfiles,NeoVim-Setup&size_weight=0.5&count_weight=0.5&hide=MATLAB,HTML,CSS,JavaScript&theme=highcontrast&hide_border=true" alt="Top Languages" style="width: 100%; max-width: 500px;">
+                    <img src="https://github-readme-stats.vercel.app/api?username=derekcorniello&show_icons=true&hide=stars&count_private=true&hide_rank=true&theme=highcontrast&hide_border=true" alt="Derek's GitHub Stats" style="width: 100%; max-width: 500px;">
                 </div>
             </Container>
 
